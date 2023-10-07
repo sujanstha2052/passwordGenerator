dc934d6c47a6335bbe5ff72ba0b0efbbd3e354f3
sujanstha2052 <sujanstha2052@gmail.com>
2023-10-07 18:30:46 +0545

    password generate and optimize using useCallback

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useEffect } from "react";
 import { useCallback } from "react";
 import { useState } from "react";
 
@@ -28,9 +29,62 @@ const App = () => {
     setPassword(generatedPassword);
   }, [length, isNumber, isCharacter, setPassword]);
 
+  useEffect(() => {
+    passwordGenerator();
+  }, [length, isNumber, isCharacter, passwordGenerator]);
+
   return (
-    <div>
-      <h1 className="text-4xl text-center text-white">Password Generator</h1>
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-800">
+      <h1 className="text-white text-center my-3">Password generator</h1>
+      <div className="flex shadow rounded-lg overflow-hidden mb-4">
+        <input
+          type="text"
+          value={password}
+          className="outline-none w-full py-1 px-3"
+          placeholder="password"
+          readOnly
+        />
+
+        <button className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0">
+          copy
+        </button>
+      </div>
+
+      <div className="flex text-sm gap-x-2">
+        <div className="flex items-center gap-x-1">
+          <input
+            className="cursor-pointer"
+            type="range"
+            min={8}
+            max={100}
+            value={length}
+            onChange={(e) => {
+              setLength(e.target.value);
+            }}
+          />
+          <label>Length: {length}</label>
+        </div>
+
+        <div className="flex items-center gap-x-1">
+          <input
+            type="checkbox"
+            id="numberInput"
+            defaultChecked={isNumber}
+            onChange={() => setIsNumber((prev) => !prev)}
+          />
+          <label>Number</label>
+        </div>
+
+        <div className="flex items-center gap-x-1">
+          <input
+            type="checkbox"
+            id="charInput"
+            defaultChecked={isCharacter}
+            onChange={() => setIsCharacter((prev) => !prev)}
+          />
+          <label>Character</label>
+        </div>
+      </div>
     </div>
   );
 };
